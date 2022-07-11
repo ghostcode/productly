b5bf6086ab4357950f2f8edca30520399ab20c15
zhuxy <ghostcode521@gmail.com>
2022-07-11 19:33:39 +0800

    add hero

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -2,15 +2,24 @@ import HeroImg from '../assets/hero.png'
 
 function Hero() {
   return (
-    <div className="lg:container lg:mx-auto">
+    <div className="lg:container lg:mx-auto flex items-center">
       <div>
-        <h3>The Design Thinking superpowers</h3>
-        <p>
+        <h3 className="mb-8 text-4xl">The Design Thinking superpowers</h3>
+        <p className="text-lg">
           Tools, tutorials, design and innovation experts, all in one place! The
           most intuitive way to imagine your next user experience.
         </p>
+        <div className="flex items-center">
+          <div className="flex items-center rounded-md bg-amber-400 border-amber-400">
+            Get started
+          </div>
+          <div>
+            <span></span>
+            <span>Watch the Video</span>
+          </div>
+        </div>
       </div>
-      <img src={HeroImg} alt="" />
+      <img className="w-2/3" src={HeroImg} alt="" />
     </div>
   );
 }
